6ad395fe41de063e39c641b5cea650b0f10055eb
zhaopigji2016 <3192086411@qq.com>
2022-02-08 13:34:39 +0800

    登录界面和主home界面

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: '登录',
+      hidden: true,
+      component: Login
+    }, {
+      path: '/home',
+      name: '',
+      component: Home,
+      hidden: true
+    }]
+})
