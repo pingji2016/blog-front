8d4c1de2f264de6d93eca06af87a9b3a81b69757
AobangLi <774164644@qq.com>
2022-02-09 22:28:53 +0800

    为了前后端分离，所有后端请求加/api前缀，在开发环境下配置proxyTable转发api请求到本机8081端口的后端，在生产环境下使用nginx转发

--- config/index.js
+++ config/index.js
@@ -11,11 +11,11 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/': {
+      '/api': {
         target: 'http://localhost:8081',
         changeOrigin: true,
         pathRewrite: {
-          '^/': ''
+          '^/api': ''
         }
       }
     },
